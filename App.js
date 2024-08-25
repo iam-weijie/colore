3c3ac42f178e50e7f8fe85fd0179f1cf77d61f1e
Weijie Wang <wang.weijie0110@gmail.com>
2024-08-25 16:33:19 -0400

    add empty_bottle on main page

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { Dimensions, Image, StyleSheet, View } from "react-native";
 import BackGround from "./components/BackGround";
 
 const Beach = require("./assets/images/beach.gif");
+const { width, height } = Dimensions.get("window");
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View>
         <BackGround placeholderImageSource={Beach} />
+        <Image
+          source={require("./assets/images/empty_bottle.png")}
+          style={styles.image}
+        ></Image>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -22,5 +27,14 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+
+  image: {
+    width: 80,
+    height: 80,
+    position: "absolute",
+    right: 0,
+    bottom: 100,
+    transform: [{ rotate: "0deg" }],
   },
 });
