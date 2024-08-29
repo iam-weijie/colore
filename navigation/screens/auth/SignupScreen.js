ec0ce78d1152e97bede64cd3bda77a1a10275847
Weijie Wang <wang.weijie0110@gmail.com>
2024-08-29 10:10:28 -0400

    fix: folder (auth) to auth for nativewind config

--- navigation/screens/auth/SignupScreen.js
+++ navigation/screens/auth/SignupScreen.js
@@ -1,17 +1,16 @@
 import React from "react";
 import { ScrollView, Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
 
 const SignupScreen = () => {
   return (
     <ScrollView className="flex-1 bg-white">
-      <SafeAreaView className="flex-1 bg-white">
-        <View className="relative w-full h-[250px]">
-          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+      <View className="flex-1 bg-white ">
+        <View className="relative w-full h-[150px]">
+          <Text className="text-2xl text-black font-semibold absolute bottom-5 left-5">
             Create Your Account
           </Text>
         </View>
-      </SafeAreaView>
+      </View>
     </ScrollView>
   );
 };
