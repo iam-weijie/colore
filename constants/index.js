4ce46d648964d3d75d4381f3c37fdec76a715e9b
Weijie Wang <wang.weijie0110@gmail.com>
2024-08-29 10:51:04 -0400

    create folder constants

---
+++ constants/index.js
@@ -0,0 +1,9 @@
+const person = require("../assets/icons/person.png");
+const email = require("../assets/icons/email.png");
+const lock = require("../assets/icons/lock.png");
+
+export const icons = {
+  person,
+  email,
+  lock,
+};
