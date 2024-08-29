04e04ed6ed9c421e744506806a08b9ce0713fa29
Weijie Wang <wang.weijie0110@gmail.com>
2024-08-29 10:53:57 -0400

    feat: add input fields for name, email and password on signup screen

--- navigation/screens/auth/SignupScreen.js
+++ navigation/screens/auth/SignupScreen.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { ScrollView, Text, View } from "react-native";
+import InputField from "../../../components/InputField";
+import { icons } from "../../../constants";
 
 const SignupScreen = () => {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
   return (
     <ScrollView className="flex-1 bg-white">
       <View className="flex-1 bg-white ">
@@ -9,6 +17,37 @@ const SignupScreen = () => {
           <Text className="text-2xl text-black font-semibold absolute bottom-5 left-5">
             Create Your Account
           </Text>
+        </View>
+
+        <View className="p-5">
+          <InputField
+            label="Name"
+            placeHolder="Enter your name"
+            icon={icons.person}
+            value={form.name}
+            onChangeText={(value) =>
+              setForm({ value: { ...form, name: value } })
+            }
+          />
+          <InputField
+            label="Email"
+            placeHolder="Enter your email"
+            icon={icons.email}
+            value={form.name}
+            onChangeText={(value) =>
+              setForm({ value: { ...form, email: value } })
+            }
+          />
+          <InputField
+            label="Password"
+            placeHolder="Enter your password"
+            icon={icons.lock}
+            value={form.name}
+            secureTextEntry={true}
+            onChangeText={(value) =>
+              setForm({ value: { ...form, password: value } })
+            }
+          />
         </View>
       </View>
     </ScrollView>
