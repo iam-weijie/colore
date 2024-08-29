50decbea13949ab9bda682bfafb8b333563a8732
Weijie Wang <wang.weijie0110@gmail.com>
2024-08-29 16:37:57 -0400

    update signup page with login with google

--- navigation/screens/auth/SignupScreen.js
+++ navigation/screens/auth/SignupScreen.js
@@ -8,7 +8,6 @@ import { icons } from "../../../constants";
 
 const SignupScreen = () => {
   const [form, setForm] = useState({
-    name: "",
     email: "",
     password: "",
   });
@@ -27,15 +26,6 @@ const SignupScreen = () => {
         </View>
 
         <View className="p-5">
-          <InputField
-            label="Name"
-            placeHolder="Enter your name"
-            icon={icons.person}
-            value={form.name}
-            onChangeText={(value) =>
-              setForm({ value: { ...form, name: value } })
-            }
-          />
           <InputField
             label="Email"
             placeHolder="Enter your email"
@@ -61,7 +51,7 @@ const SignupScreen = () => {
           <CustomButton
             title="Sign Up"
             onPress={onSignUpPress}
-            className="mt-6"
+            className="mt-12"
           ></CustomButton>
 
           <OAuth />
