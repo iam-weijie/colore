30cb4dec1f18a6562d60c4f1cef6c663d6122f82
Weijie Wang <wang.weijie0110@gmail.com>
2024-08-29 09:33:47 -0400

    signup screen

--- navigation/screens/(auth)/SignupScreen.js
+++ navigation/screens/(auth)/SignupScreen.js
@@ -1,14 +1,19 @@
 import React from "react";
-import { ScrollView, StyleSheet, Text } from "react-native";
+import { ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const SignupScreen = () => {
   return (
-    <ScrollView>
-      <Text>SignupScreen</Text>
+    <ScrollView className="flex-1 bg-white">
+      <SafeAreaView className="flex-1 bg-white">
+        <View className="relative w-full h-[250px]">
+          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+            Create Your Account
+          </Text>
+        </View>
+      </SafeAreaView>
     </ScrollView>
   );
 };
 
 export default SignupScreen;
-
-const styles = StyleSheet.create({});
