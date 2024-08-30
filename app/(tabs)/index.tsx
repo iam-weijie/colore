4ccb88c3d237edb0490bd9bd0a7dbbd83ac1bb66
Weijie Wang <wang.weijie0110@gmail.com>
2024-08-30 09:57:22 -0400

    config nativewind

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,12 @@
-import { StatusBar, Text, View } from "react-native";
+import { SafeAreaView, StatusBar, Text } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
+      <Text className="text-red-500">
+        Open up App.js to start working on your app!
+      </Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
