8f4769dc5756b93800c89c628b4ce2d41d845a25
Weijie Wang <wang.weijie0110@gmail.com>
2024-08-30 11:56:45 -0400

    final version of signup screen img

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -6,10 +6,7 @@ const SignUp = () => {
     <ScrollView className="flex-1 bg-white">
       <View className="flex-1 bg-white ">
         <View className="relative w-full h-[250px]">
-          <Image
-            source={images.signup}
-            className="z-0 w-full h-[200px] "
-          />
+          <Image source={images.signup} className="z-0 w-full h-[250px] " />
           <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
             Create Your Account
           </Text>
