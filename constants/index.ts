e2b9222a62defa0e2446eeb4ddbbc73a89a7bf89
Weijie Wang <wang.weijie0110@gmail.com>
2024-08-30 18:51:54 -0400

    update index page in constants

---
+++ constants/index.ts
@@ -0,0 +1,18 @@
+import check from "@/assets/icons/check.png";
+import email from "@/assets/icons/email.png";
+import google from "@/assets/icons/google.png";
+import lock from "@/assets/icons/lock.png";
+import person from "@/assets/icons/person.png";
+import signup from "@/assets/images/signup.png";
+
+export const icons = {
+  person,
+  email,
+  lock,
+  google,
+  check,
+};
+
+export const images = {
+  signup,
+};
