07e2d0711aba383c3b18ad77c5334e83e9c0fb45
Weijie Wang <wang.weijie0110@gmail.com>
2024-08-30 19:07:51 -0400

    log in page

--- app/(auth)/log-in.tsx
+++ app/(auth)/log-in.tsx
@@ -1,10 +1,67 @@
-import { Text, View } from "react-native";
+import CustomButton from "@/components/CustomButton";
+import InputField from "@/components/InputField";
+import OAuth from "@/components/OAuth";
+import { icons, images } from "@/constants";
+import { Link } from "expo-router";
+import { useState } from "react";
+import { Image, ScrollView, Text, View } from "react-native";
 
 const LogIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const onLogInPress = async () => {};
+
   return (
-    <View>
-      <Text>Log In</Text>
-    </View>
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white ">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.login} className="z-0 w-full h-[250px] " />
+          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+            Welcome 👋
+          </Text>
+        </View>
+
+        <View className="p-5">
+          <InputField
+            label="Email"
+            placeholder="Enter your email"
+            icon={icons.email}
+            textContentType="emailAddress"
+            value={form.email}
+            onChangeText={(value) => setForm({ ...form, email: value })}
+          />
+          <InputField
+            label="Password"
+            placeholder="Enter your password"
+            icon={icons.lock}
+            value={form.password}
+            secureTextEntry={true}
+            textContentType="password"
+            onChangeText={(value) => setForm({ ...form, password: value })}
+          />
+
+          <CustomButton
+            title="Log In"
+            onPress={onLogInPress}
+            className="mt-12"
+          />
+
+          <OAuth />
+
+          <Text className="text-base text-center text-general-200 mt-10">
+            Don't have an account?{" "}
+            <Link href="/sign-up">
+              <Text className="text-primary-500">Sign Up</Text>
+            </Link>
+          </Text>
+        </View>
+
+        {/*Verification modal*/}
+      </View>
+    </ScrollView>
   );
 };
 
--- constants/index.ts
+++ constants/index.ts
@@ -3,6 +3,7 @@ import email from "@/assets/icons/email.png";
 import google from "@/assets/icons/google.png";
 import lock from "@/assets/icons/lock.png";
 import person from "@/assets/icons/person.png";
+import login from "@/assets/images/login.png";
 import signup from "@/assets/images/signup.png";
 
 export const icons = {
@@ -15,4 +16,5 @@ export const icons = {
 
 export const images = {
   signup,
+  login,
 };
