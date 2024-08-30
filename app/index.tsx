d3b80167cf9039de0cdf307611a95646d0157ccd
Weijie Wang <wang.weijie0110@gmail.com>
2024-08-30 10:46:26 -0400

    change route to signup page when open app

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,7 @@
-import { Text, View } from "react-native";
+import { Redirect } from "expo-router";
 
 const Home = () => {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  );
+  return <Redirect href="/(auth)/sign-up" />;
 };
 
 export default Home;
