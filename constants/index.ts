996d47b5d9cecf8203b5b25d28b24e37f8a58ad8
Weijie Wang <wang.weijie0110@gmail.com>
2024-08-31 18:22:29 -0400

    add some icons

--- constants/index.ts
+++ constants/index.ts
@@ -1,11 +1,17 @@
+import album from "@/assets/icons/album.png";
 import chat from "@/assets/icons/chat.png";
 import check from "@/assets/icons/check.png";
+import close from "@/assets/icons/close.png";
 import email from "@/assets/icons/email.png";
 import google from "@/assets/icons/google.png";
 import home from "@/assets/icons/home.png";
 import lock from "@/assets/icons/lock.png";
+import logout from "@/assets/icons/logout.png";
 import person from "@/assets/icons/person.png";
+import pin from "@/assets/icons/pin.png";
 import profile from "@/assets/icons/profile.png";
+import search from "@/assets/icons/search.png";
+
 import login from "@/assets/images/login.png";
 import signup from "@/assets/images/signup.png";
 
@@ -16,8 +22,13 @@ export const icons = {
   google,
   check,
   home,
+  pin,
   chat,
+  close,
   profile,
+  album,
+  logout,
+  search,
 };
 
 export const images = {
