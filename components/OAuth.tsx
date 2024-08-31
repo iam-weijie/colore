7c5a729c33844a63a7d2ec41cd456bd9c598b6c4
Weijie Wang <wang.weijie0110@gmail.com>
2024-08-31 16:45:08 -0400

    google login

--- components/OAuth.tsx
+++ components/OAuth.tsx
@@ -1,9 +1,27 @@
 import { icons } from "@/constants";
-import { Image, Text, View } from "react-native";
+import { googleOAuth } from "@/lib/auth";
+import { useOAuth } from "@clerk/clerk-expo";
+import { router } from "expo-router";
+import { useCallback } from "react";
+import { Alert, Image, Text, View } from "react-native";
 import CustomButton from "./CustomButton";
 
 const OAuth = () => {
-  const handleGoogleLogIn = async () => {};
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+
+  const handleGoogleLogIn = useCallback(async () => {
+    try {
+      const result = await googleOAuth(startOAuthFlow);
+
+      if (result.code === "session_exists") {
+        Alert.alert("Success", "Session Exists. Redirecting to home page");
+        router.push("/(root)/(tabs)/home");
+      }
+      Alert.alert(result.success ? "Success" : "Error", result.message);
+    } catch (err) {
+      console.error("OAuth error", err);
+    }
+  }, []);
 
   return (
     <View>
