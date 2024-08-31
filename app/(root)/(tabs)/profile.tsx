dbc0273b5598b9f6900061b841296b04b20fc2ec
Weijie Wang <wang.weijie0110@gmail.com>
2024-08-31 19:15:35 -0400

    profile screen with possibilty to change name

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,13 +1,12 @@
-import InputField from "@/components/InputField";
 import { useUser } from "@clerk/clerk-expo";
 import { useState } from "react";
-import { ScrollView, Text, View } from "react-native";
+import { ScrollView, Text, TextInput, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Profile = () => {
   const { user } = useUser();
 
-  const [name, setName] = useState(user?.firstName);
+  const [name, setName] = useState(user?.firstName || "J. Doe");
   const [isEditing, setIsEditing] = useState(false);
 
   return (
@@ -16,37 +15,19 @@ const Profile = () => {
         className="px-5"
         contentContainerStyle={{ paddingBottom: 120 }}
       >
-        <Text className="text-2xl font-JakartaBold my-5">{name}</Text>
-
-        <View className="flex flex-col items-start justify-center bg-white rounded-lg shadow-sm shadow-neutral-300 px-5 py-3">
-          <View className="flex flex-col items-start justify-start w-full">
-            <InputField
-              label="First name"
-              placeholder={user?.firstName || "Not Found"}
-              containerStyle="w-full"
-              inputStyle="p-3.5"
-              editable={false}
-            />
-
-            <InputField
-              label="Last name"
-              placeholder={user?.lastName || "Not Found"}
-              containerStyle="w-full"
-              inputStyle="p-3.5"
-              editable={false}
-            />
-
-            <InputField
-              label="Email"
-              placeholder={
-                user?.primaryEmailAddress?.emailAddress || "Not Found"
-              }
-              containerStyle="w-full"
-              inputStyle="p-3.5"
-              editable={false}
-            />
-          </View>
-        </View>
+        {isEditing ? (
+          <TextInput
+            className="text-2xl font-JakartaBold my-5"
+            placeholder={name}
+            onChangeText={setName}
+            onBlur={() => setIsEditing(false)}
+            autoFocus
+          />
+        ) : (
+          <TouchableOpacity onPress={() => setIsEditing(true)}>
+            <Text className="text-2xl font-JakartaBold my-5">{name}</Text>
+          </TouchableOpacity>
+        )}
       </ScrollView>
     </SafeAreaView>
   );
