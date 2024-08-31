87501a73a60576dd41314e24a59adcbc3b396321
Weijie Wang <wang.weijie0110@gmail.com>
2024-08-31 10:07:57 -0400

    add tab bar

--- constants/index.ts
+++ constants/index.ts
@@ -1,8 +1,11 @@
+import chat from "@/assets/icons/chat.png";
 import check from "@/assets/icons/check.png";
 import email from "@/assets/icons/email.png";
 import google from "@/assets/icons/google.png";
+import home from "@/assets/icons/home.png";
 import lock from "@/assets/icons/lock.png";
 import person from "@/assets/icons/person.png";
+import profile from "@/assets/icons/profile.png";
 import login from "@/assets/images/login.png";
 import signup from "@/assets/images/signup.png";
 
@@ -12,6 +15,9 @@ export const icons = {
   lock,
   google,
   check,
+  home,
+  chat,
+  profile,
 };
 
 export const images = {
