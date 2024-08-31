b9956cb7923adb4f6bae91bb4e558caf8de53843
Weijie Wang <wang.weijie0110@gmail.com>
2024-08-30 21:51:58 -0400

    fix: blank page issue, import from clerk-expo

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,4 @@
-import { ClerkLoaded, ClerkProvider } from "@clerk/clerk-react";
+import { ClerkLoaded, ClerkProvider } from "@clerk/clerk-expo";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
