15adf7e2e1fee247749eb985bc70094338f170e1
Weijie Wang <wang.weijie0110@gmail.com>
2024-08-30 21:42:31 -0400

    fix small bug

--- app/index.tsx
+++ app/index.tsx
@@ -4,9 +4,7 @@ import { Redirect } from "expo-router";
 const Home = () => {
   const { isSignedIn } = useAuth();
 
-  if (isSignedIn) {
-    return <Redirect href={"/(root)/(tabs)/home"} />;
-  }
+  if (isSignedIn) return <Redirect href="/(root)/(tabs)/home" />;
 
   return <Redirect href="/(auth)/sign-up" />;
 };
