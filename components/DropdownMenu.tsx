647d049aa54ec5a3bf8f31a1acd0f0751de82c2c
Weijie Wang <wang.weijie0110@gmail.com>
2024-09-07 20:58:02 -0400

    dropdown menu in profile screen

--- components/DropdownMenu.tsx
+++ components/DropdownMenu.tsx
@@ -1,32 +1,11 @@
+import { data } from "@/constants";
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { Dropdown } from "react-native-element-dropdown";
-
-const data = [
-  { label: "Item 1", value: "1" },
-  { label: "Item 2", value: "2" },
-  { label: "Item 3", value: "3" },
-  { label: "Item 4", value: "4" },
-  { label: "Item 5", value: "5" },
-  { label: "Item 6", value: "6" },
-  { label: "Item 7", value: "7" },
-  { label: "Item 8", value: "8" },
-];
 
 const DropdownMenu = () => {
   const [value, setValue] = useState(null);
   const [isFocus, setIsFocus] = useState(false);
-
-  const renderLabel = () => {
-    if (value || isFocus) {
-      return (
-        <Text style={[styles.label, isFocus && { color: "blue" }]}>
-          Dropdown label
-        </Text>
-      );
-    }
-    return null;
-  };
 
   return (
     <View style={styles.container}>
@@ -59,11 +38,11 @@ export default DropdownMenu;
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "white",
-    padding: 16,
+    width: "90%",
+    padding: 0,
   },
   dropdown: {
-    height: 50,
+    height: 35,
     borderColor: "gray",
     borderWidth: 0.5,
     borderRadius: 8,
