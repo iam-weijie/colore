4aedc7f2471bc2a62e01e16c5477eb056af78b13
Weijie Wang <wang.weijie0110@gmail.com>
2024-09-10 23:31:24 -0400

    add check when a city is selected

--- app/(root)/city.tsx
+++ app/(root)/city.tsx
@@ -1,5 +1,6 @@
 import { countries } from "@/constants/index";
 import { useLocalSearchParams } from "expo-router";
+import { useState } from "react";
 import { FlatList, Text, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -9,6 +10,12 @@ const City = () => {
   const selectedCountry = countries.find((c) => c.name === country);
   const selectedState = selectedCountry?.states.find((s) => s.name === state);
   const cities = selectedState ? selectedState.cities : [];
+
+  const [selectedCity, setSelectedCity] = useState("");
+
+  const handleCityPress = (city: string) => {
+    setSelectedCity(city);
+  };
 
   return (
     <SafeAreaView className="flex-1">
@@ -20,15 +27,20 @@ const City = () => {
         keyExtractor={(item) => item}
         renderItem={({ item }) => (
           <TouchableOpacity
-          // TODO: store the selected info to db
-          // onPress={() =>
+            onPress={() => handleCityPress(item)}
+            // TODO: store the selected info to db
+            // onPress={() =>
 
-          //     params: { city: item, state, country },
-          //   })
+            //     params: { city: item, state, country },
+            //   })
           >
             <Text className="font-JakartaSemiBold text-[15px] ml-3 my-2">
               {item}
             </Text>
+
+            {selectedCity === item && (
+              <Text className="absolute text-lg my-1 mx-3 right-0">✓</Text>
+            )}
           </TouchableOpacity>
         )}
       />
