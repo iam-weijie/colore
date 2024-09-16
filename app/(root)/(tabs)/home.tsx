066bd07462abe64dbe5b98d32ddf64fb35a8859d
Emily Wang <ewang0332@gmail.com>
2024-09-15 20:00:42 -0400

    users can new submit posts

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,15 +1,22 @@
 import { SignedIn, useUser } from "@clerk/clerk-expo";
-import { Text } from "react-native";
+import { Text, Button, View } from "react-native";
+import { router } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
+import CustomButton from "@/components/CustomButton";
 
 export default function Page() {
   const { user } = useUser();
+
+  const handleNewPostPress = () => {
+    router.push("/(root)/new-post");
+  };
 
   return (
     <SafeAreaView>
       <SignedIn>
         <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+        <Button title="New Post" onPress={handleNewPostPress} />
       </SignedIn>
     </SafeAreaView>
   );
-}
+};
---
+++ app/(root)/new-post.tsx
@@ -0,0 +1,78 @@
+import { router } from "expo-router";
+import { SignedIn, useUser } from "@clerk/clerk-expo";
+import { TextInput, Text, StyleSheet, Alert } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import React, { useState } from "react";
+import { fetchAPI } from "@/lib/fetch";
+import CustomButton from "@/components/CustomButton";
+
+const NewPost = () => {
+  const { user } = useUser();
+  const [postContent, setPostContent] = useState("");
+
+  const handlePostSubmit = async () => {
+    const cleanedContent = postContent.trim();
+    console.log(user!.id);
+    // additional sanitization needed
+
+    if (cleanedContent === "") {
+      Alert.alert("Error", "Post content cannot be empty.");
+      return;
+    }
+
+    await fetchAPI("/(api)/post", {
+      method: "POST",
+      body: JSON.stringify({
+        content: cleanedContent,
+        clerkId: user!.id,
+      }),
+    });
+
+    setPostContent("");
+    Alert.alert("Success", "Post created successfully.");
+
+    router.replace("/(root)/home");
+  };
+
+  return (
+    <SafeAreaView className="flex-1">
+      <SignedIn>
+        <Text className="text-lg font-JakartaSemiBold m-3">
+          Create a New Post
+        </Text>
+        <TextInput
+          placeholder="Enter post content"
+          value={postContent}
+          onChangeText={setPostContent}
+          multiline
+          style={styles.textInput}
+        />
+        <CustomButton title="Submit Post" onPress={handlePostSubmit} />
+      </SignedIn>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 16,
+  },
+  textInput: {
+    height: 150,
+    width: "100%",
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 4,
+    padding: 8,
+    marginBottom: 16,
+  },
+});
+
+export default NewPost;
