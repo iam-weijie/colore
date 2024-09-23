1488e645f81d47691162fbfada084e42fdf9ff09
Arthur <arthur.descrivan@gmail.com>
2024-09-23 14:28:42 -0400

    <Actually added content into the API (forgot to save on last commit>

---
+++ app/(api)/userInfoCheck+api.ts
@@ -0,0 +1,44 @@
+import { neon } from "@neondatabase/serverless";
+
+export async function GET(request: Request) {
+  try {
+    const url = new URL(request.url);
+    const clerkId = url.searchParams.get("id");
+    if (!clerkId) {
+      return new Response(JSON.stringify({ error: "User ID is required" }), {
+        status: 400,
+      });
+    }
+
+    const sql = neon(`${process.env.DATABASE_URL}`);
+
+    const response = await sql`
+      SELECT
+      firstname,
+      lastname,
+      date_of_birth,
+      city,
+      state,
+      country      
+      FROM users WHERE clerk_id = ${clerkId};
+    `;
+
+    if (response.length === 0) {
+      return new Response(JSON.stringify({ error: "User not found" }), {
+        status: 404,
+      });
+    }
+
+    return new Response(JSON.stringify({ data: response[0] }), {
+      status: 200,
+    });
+  } catch (error) {
+    console.error(error);
+    return new Response(
+      JSON.stringify({ error: "Failed to fetch user info" }),
+      {
+        status: 500,
+      }
+    );
+  }
+}
