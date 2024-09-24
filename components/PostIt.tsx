a3734c0a59ead47943df915f4ff4c56e45e9e3f9
Weijie Wang <wang.weijie0110@gmail.com>
2024-09-24 01:43:31 -0400

    postit componenet change, fix shadow effect on cut corner

--- components/PostIt.tsx
+++ components/PostIt.tsx
@@ -2,9 +2,15 @@ import { View } from "react-native";
 
 const PostIt = () => {
   return (
-    <View className="bg-yellow-300 rounded-2xl w-40 h-40 shadow-lg absolute left-20 top-36">
+    <View className="absolute left-10 top-36">
+      {/* Bottom Half with Shadow */}
+      <View className="bg-yellow-300 rounded-b-2xl w-40 h-32 absolute bottom-0 shadow-lg" />
+
+      {/* Top Half without Shadow */}
+      <View className="bg-yellow-300 rounded-2xl w-40 h-40" />
+
       {/* Folded Corner */}
-      <View className="absolute right-0 top-0 w-0 h-0 border-t-[32px] border-l-[32px] rounded-bl-md border-t-general-300 border-l-yellow-200 border-r-transparent" />
+      <View className="absolute right-0 top-0 w-0 h-0 border-t-[36px] border-l-[36px] rounded-bl-md border-t-[#f2f2f2] border-l-yellow-200 border-r-transparent" />
     </View>
   );
 };
