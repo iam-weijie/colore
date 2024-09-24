43f3114d652091770d55b49c13da0a8b3830796b
Weijie Wang <wang.weijie0110@gmail.com>
2024-09-24 11:44:33 -0400

    add pencil and refresh icon

--- constants/index.ts
+++ constants/index.ts
@@ -7,9 +7,11 @@ import google from "@/assets/icons/google.png";
 import home from "@/assets/icons/home.png";
 import lock from "@/assets/icons/lock.png";
 import logout from "@/assets/icons/logout.png";
+import pencil from "@/assets/icons/pencil.png";
 import person from "@/assets/icons/person.png";
 import pin from "@/assets/icons/pin.png";
 import profile from "@/assets/icons/profile.png";
+import refresh from "@/assets/icons/refresh.png";
 import search from "@/assets/icons/search.png";
 import trash from "@/assets/icons/trash.png";
 
@@ -31,6 +33,8 @@ export const icons = {
   logout,
   search,
   trash,
+  pencil,
+  refresh,
 };
 
 export const images = {
