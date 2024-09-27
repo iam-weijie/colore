9f019a258604a014456fc4c665dea8f670af4e0b
Weijie Wang <wang.weijie0110@gmail.com>
2024-09-27 11:03:33 -0400

    move city, country, state into location folder

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -5,9 +5,7 @@ const Layout = () => {
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="user-info" options={{ headerShown: false }} />
-      <Stack.Screen name="country" options={{ headerShown: false }} />
-      <Stack.Screen name="state" options={{ headerShown: false }} />
-      <Stack.Screen name="city" options={{ headerShown: false }} />
+      <Stack.Screen name="(location)" options={{ headerShown: false }} />
     </Stack>
   );
 };
