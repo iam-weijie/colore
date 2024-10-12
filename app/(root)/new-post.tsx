3d4bd52a009a0b42774e76b342180576b7090b10
mintary <ewang0332@gmail.com>
2024-10-11 20:51:50 -0400

    text field ui adjusted, grows and shrinks with user input

--- app/(root)/new-post.tsx
+++ app/(root)/new-post.tsx
@@ -1,19 +1,25 @@
-import CustomButton from "@/components/CustomButton";
 import { fetchAPI } from "@/lib/fetch";
 import { SignedIn, useUser } from "@clerk/clerk-expo";
 import { router } from "expo-router";
 import React, { useState } from "react";
-import { Alert, Text, TextInput } from "react-native";
+import { Alert, Text, TextInput, View, Button, Dimensions } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const NewPost = () => {
   const { user } = useUser();
   const [postContent, setPostContent] = useState("");
+  const [inputHeight, setInputHeight] = useState(40);
+
+  // need to get user's screen size to set a min height
+  const minHeight = Dimensions.get("screen").height * 0.2; 
+
+  const handleContentSizeChange = (event: any) => {
+    setInputHeight(event.nativeEvent.contentSize.height);
+    
+  };
 
   const handlePostSubmit = async () => {
     const cleanedContent = postContent.trim();
-    // additional sanitization needed
-
     if (cleanedContent === "") {
       Alert.alert("Error", "Post content cannot be empty.");
       return;
@@ -36,16 +42,31 @@ const NewPost = () => {
   return (
     <SafeAreaView className="flex-1">
       <SignedIn>
-        <Text className="text-lg font-JakartaSemiBold m-3">
+        <Text className="text-xl font-JakartaSemiBold m-3">
           Create a New Post
         </Text>
-        <TextInput
-          placeholder="Enter post content"
-          value={postContent}
-          onChangeText={setPostContent}
-          multiline
+        <View className="border mx-3 my-5 rounded-lg border-slate-400 h-50 max-h-[70%]">
+          <TextInput
+            className="font-Jakarta mx-2 my-2"
+            placeholder="Enter post content"
+            value={postContent}
+            onChangeText={setPostContent}
+            multiline
+            scrollEnabled
+            onContentSizeChange={handleContentSizeChange}
+            style={{ 
+              paddingTop: 10, 
+              paddingBottom: 0, 
+              minHeight: minHeight,
+              textAlignVertical: "top",
+            }}
+          />
+        </View>
+        <Button 
+          className="bg-blue-500 text-white text-sm px-3 py-1 rounded-full absolute right-4 bottom-4"
+          title="Submit Post" 
+          onPress={handlePostSubmit} 
         />
-        <CustomButton title="Submit Post" onPress={handlePostSubmit} />
       </SignedIn>
     </SafeAreaView>
   );
