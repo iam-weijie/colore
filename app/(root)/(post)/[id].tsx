c2d9293e81d36e1ea14880308b5e21950dfb8817
mintary <ewang0332@gmail.com>
2024-10-17 16:40:02 -0400

    feat: users can now leave comments

--- app/(root)/(post)/[id].tsx
+++ app/(root)/(post)/[id].tsx
@@ -40,6 +40,9 @@ const PostScreen  = () => {
   const [error, setError] = useState<string | null>(null);
   const [postComments, setPostComments] = useState<PostComment[]>([]);
   const [likedPost, setLikedPost] = useState<boolean>(false);
+  const [newComment, setNewComment] = useState<string>("");
+
+  const { height } = Dimensions.get("window");
 
   const fetchComments = async () => {
     setLoading(true);
@@ -63,6 +66,27 @@ const PostScreen  = () => {
       setLoading(false);
     }
   };
+
+  const handleCommentSubmit = async () => {
+    if (!newComment.trim()) return;
+
+    try {
+      const response = await fetchAPI("/(api)/(comments)/newComment", {
+        method: "POST",
+        body: JSON.stringify({ content: newComment, postId: id, clerkId: user?.id }),
+      });
+
+      if (response.error) {
+        throw new Error(response.error);
+      }
+      setNewComment("");
+      fetchComments();
+    } catch (error) {
+      Alert.alert("Error", "Failed to submit comment.");
+      console.error("Failed to submit comment:", error);
+    }
+  };
+
 
   const handleDeletePress = async () => {
     Alert.alert("Delete Post", "Are you sure you want to delete this post?", [
@@ -110,65 +134,80 @@ const PostScreen  = () => {
           onPress={() => Keyboard.dismiss()}
           onPressIn={() => Keyboard.dismiss()}
         >
-          <ScrollView>
-            <View className="flex flex-row justify-center items-center mt-3 mx-4">
-              <View className="flex-1">
-                <TouchableOpacity onPress={() => router.back()}>
-                  <AntDesign name="caretleft" size={18} color="0076e3" />
-                </TouchableOpacity>
-              </View>
-              <Text className="absolute text-xl font-JakartaSemiBold">
-                Post
-              </Text>
+          <View className="flex flex-row justify-center items-center mt-3 mx-4">
+            <View className="flex-1">
+              <TouchableOpacity onPress={() => router.back()}>
+                <AntDesign name="caretleft" size={18} color="0076e3" />
+              </TouchableOpacity>
             </View>
-  
-            {/* Post information */}
-            <View className="p-4 border-b border-gray-200">
-              <Text className="font-JakartaSemiBold text-lg">
-                {nickname || firstname}
-              </Text>
-              <Text className="text-sm text-gray-500">
-                {typeof created_at === "string" ? new Date(created_at).toLocaleString() : "No date"}
-              </Text>
-              <Text className="mt-2">{content}</Text>
-  
-              <View className="my-2 flex-row justify-between items-center">
-                <TouchableOpacity onPress={() => setLikedPost(!likedPost)}>
-                  <MaterialCommunityIcons
-                    name={likedPost ? "heart" : "heart-outline"}
-                    size={32}
-                    color={likedPost ? "red" : "black"}
-                  />
-                </TouchableOpacity>
-                {clerk_id === user?.id && (
-                  <TouchableOpacity onPress={handleDeletePress}>
-                    <Image source={icons.trash} className="w-7 h-7" />
-                  </TouchableOpacity>
-                )}
-              </View>
-            </View>
-
-            {/* Comment section */}
-            <View className="mt-4">
-              <Text className="font-JakartaSemiBold text-lg mx-4">Comments</Text>
-              {loading && (
-                <ActivityIndicator size="large" color="#0076e3" />
-              )}
-              {error && (
-                <Text className="text-red-500 mx-4">{error}</Text>
-              )}
-              {!loading && !error && postComments.length === 0 && (
-                <Text className="text-gray-500 mx-4 min-h-[30px]">No comments yet.</Text>
-              )}
-              {!loading && !error && postComments.length > 0 && (
-                <View className="mx-2">
-                  {postComments.map((comment) => renderComment({ item: comment }))}
-                </View>
-              )}
-            </View>
-                
-          </ScrollView>
+            <Text className="absolute text-xl font-JakartaSemiBold">
+              Post
+            </Text>
+          </View>
         </TouchableWithoutFeedback>
+        <ScrollView>
+          {/* Post information */}
+          <View className="p-4 border-b border-gray-200">
+            <Text className="font-JakartaSemiBold text-lg">
+              {nickname || firstname}
+            </Text>
+            <Text className="text-sm text-gray-500">
+              {typeof created_at === "string" ? new Date(created_at).toLocaleString() : "No date"}
+            </Text>
+            <Text className="mt-2">{content}</Text>
+
+            <View className="my-2 flex-row justify-between items-center">
+              <TouchableOpacity onPress={() => setLikedPost(!likedPost)}>
+                <MaterialCommunityIcons
+                  name={likedPost ? "heart" : "heart-outline"}
+                  size={32}
+                  color={likedPost ? "red" : "black"}
+                />
+              </TouchableOpacity>
+              {clerk_id === user?.id && (
+                <TouchableOpacity onPress={handleDeletePress}>
+                  <Image source={icons.trash} className="w-7 h-7" />
+                </TouchableOpacity>
+              )}
+            </View>
+          </View>
+
+          {/* Comment section */}
+          <View className="mt-4">
+            <Text className="font-JakartaSemiBold text-lg mx-4">Comments</Text>
+            {loading && (
+              <ActivityIndicator size="large" color="#0076e3" />
+            )}
+            {error && (
+              <Text className="text-red-500 mx-4">{error}</Text>
+            )}
+            {!loading && !error && postComments.length === 0 && (
+              <Text className="text-gray-500 mx-4 min-h-[30px]">No comments yet.</Text>
+            )}
+            {!loading && !error && postComments.length > 0 && (
+              <View className="mx-2">
+                {postComments.map((comment) => renderComment({ item: comment }))}
+              </View>
+            )}
+          </View>
+        </ScrollView>
+        <View className="flex-row justify-between items-center absolute bottom-0 left-0 right-0 p-4 bg-white border-t border-gray-200">
+          <TextInput
+            placeholder="Write a comment..."
+            value={newComment}
+            onChangeText={setNewComment}
+            onSubmitEditing={handleCommentSubmit}
+            multiline={true}
+          />
+          <CustomButton
+            title="Send"
+            onPress={handleCommentSubmit} 
+            disabled={newComment.length === 0}
+            className="absolute bottom-4 right-4 w-14 h-8 rounded-md"
+            fontSize="sm"
+            padding="0"
+          />
+        </View>
       </SignedIn>
     </SafeAreaView>
   );  
