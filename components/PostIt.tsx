df7f720bdc7c61695926e44700b9b23016a277d6
Weijie Wang <wang.weijie0110@gmail.com>
2024-10-27 23:31:56 -0400

    use tailwind

--- components/PostIt.tsx
+++ components/PostIt.tsx
@@ -3,42 +3,27 @@ import Svg, { Path } from "react-native-svg";
 
 const PostIt = () => {
   return (
-    <View style={{ width: 160, height: 160 }}>
-      {/* Main Post-it with Shadow */}
-      <View
-        style={{
-          width: 160,
-          height: 160,
-          shadowColor: "#000",
-          shadowOffset: { width: 0, height: 4 },
-          shadowOpacity: 0.2,
-          shadowRadius: 4,
-          elevation: 4,
-        }}
-      >
-        {/* Post-it Shape */}
-        <Svg height="160" width="160">
-          <Path
-            d="M16,0 
-               A16,16 0 0,0 0,16
-               L0,144
-               A16,16 0 0,0 16,160
-               L144,160
-               A16,16 0 0,0 160,144
-               L160,36
-               L124,0
-               Z"
-            fill="#fde047"
-          />
-        </Svg>
-      </View>
+    <View className="w-40 h-40 shadow">
+      {/* Post-it Shape */}
+      <Svg height="160" width="160">
+        <Path
+          d="M16,0 
+             A16,16 0 0,0 0,16
+             L0,144
+             A16,16 0 0,0 16,160
+             L144,160
+             A16,16 0 0,0 160,144
+             L160,36
+             L124,0
+             Z"
+          fill="#fde047" // Tailwind yellow-300
+        />
+      </Svg>
 
       {/* Folded Corner */}
       <View
+        className="absolute right-0 top-0"
         style={{
-          position: "absolute",
-          right: 0,
-          top: 0,
           width: 0,
           height: 0,
           borderTopWidth: 36,
@@ -46,7 +31,7 @@ const PostIt = () => {
           borderRightWidth: 0,
           borderStyle: "solid",
           borderTopColor: "transparent",
-          borderLeftColor: "#fef08a",
+          borderLeftColor: "#fef08a", // Tailwind yellow-200
           borderRightColor: "transparent",
         }}
       />
