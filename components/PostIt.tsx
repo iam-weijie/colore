9aeaa6fbddb48207bebfc2571e10b55c54cc78a2
Weijie Wang <wang.weijie0110@gmail.com>
2024-10-27 23:25:37 -0400

    fix: yellow corner and shadow

--- components/PostIt.tsx
+++ components/PostIt.tsx
@@ -4,64 +4,52 @@ import Svg, { Path } from "react-native-svg";
 const PostIt = () => {
   return (
     <View style={{ width: 160, height: 160 }}>
-      {/* Container with Shadow */}
-      <View style={{
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: 160,
-        height: 160,
-        shadowColor: "#000",
-        shadowOffset: { width: 0, height: 4 },
-        shadowOpacity: 0.2,
-        shadowRadius: 4,
-        elevation: 4,
-      }}>
-        {/* Bottom Half */}
-        <View style={{
-          position: 'absolute',
-          bottom: 0,
+      {/* Main Post-it with Shadow */}
+      <View
+        style={{
           width: 160,
-          height: 128,
-          backgroundColor: '#fde047', // yellow-300
-          borderBottomLeftRadius: 16,
-          borderBottomRightRadius: 16,
-        }} />
-
-        {/* Main Square with All Rounded Corners */}
+          height: 160,
+          shadowColor: "#000",
+          shadowOffset: { width: 0, height: 4 },
+          shadowOpacity: 0.2,
+          shadowRadius: 4,
+          elevation: 4,
+        }}
+      >
+        {/* Post-it Shape */}
         <Svg height="160" width="160">
           <Path
             d="M16,0 
                A16,16 0 0,0 0,16
                L0,144
-               A16,16 0 0,1 16,160
+               A16,16 0 0,0 16,160
                L144,160
-               A16,16 0 0,1 160,144
+               A16,16 0 0,0 160,144
                L160,36
                L124,0
-               A16,16 0 0,0 16,0
                Z"
-            fill="#fde047" // yellow-300
+            fill="#fde047"
           />
         </Svg>
       </View>
 
-      {/* Folded Corner Shadow */}
-      <View style={{
-        position: 'absolute',
-        right: 0,
-        top: 0,
-        width: 0,
-        height: 0,
-        borderTopWidth: 36,
-        borderLeftWidth: 36,
-        borderRightWidth: 0,
-        borderStyle: 'solid',
-        borderTopColor: 'transparent',
-        borderLeftColor: '#fef08a', // yellow-200
-        borderRightColor: 'transparent',
-        zIndex: 1,
-      }} />
+      {/* Folded Corner */}
+      <View
+        style={{
+          position: "absolute",
+          right: 0,
+          top: 0,
+          width: 0,
+          height: 0,
+          borderTopWidth: 36,
+          borderLeftWidth: 36,
+          borderRightWidth: 0,
+          borderStyle: "solid",
+          borderTopColor: "transparent",
+          borderLeftColor: "#fef08a",
+          borderRightColor: "transparent",
+        }}
+      />
     </View>
   );
 };
