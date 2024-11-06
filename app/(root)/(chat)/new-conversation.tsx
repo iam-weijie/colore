71cf879eb134aab4390f5773c3e53c8d6f8bde73
Arthur <arthur.descrivan@gmail.com>
2024-11-06 08:57:19 -0500

    Merge remote-tracking branch 'origin/main' into Hard-coding-everything-for-demo

--- app/(root)/(chat)/new-conversation.tsx
+++ app/(root)/(chat)/new-conversation.tsx
@@ -7,40 +7,40 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-import { User } from "../../../types/type";
+import { UserProfileType } from "@/types/type";
 
 const NewConversation = (): React.ReactElement => {
   const [searchText, setSearchText] = useState("");
-  const [users, setUsers] = useState<User[]>([]);
+  const [users, setUsers] = useState<UserProfileType[]>([]);
 
   const filteredUsers = users.filter((user) =>
     (
-      user.first_name.toLowerCase() +
+      user.firstname.toLowerCase() +
       " " +
-      user.last_name.toLowerCase()
+      user.lastname.toLowerCase()
     ).includes(searchText.toLowerCase())
   );
 
-  const startChat = (user: User): void => {
-    console.log(`Starting chat with ${user.first_name} ${user.last_name}`);
+  const startChat = (user: UserProfileType): void => {
+    console.log(`Starting chat with ${user.firstname} ${user.lastname}`);
     // TODO: Add your logic for initiating a chat here
   };
-  const showProfile = (user: User): void => {
-    console.log(`Showing profile of ${user.first_name} ${user.last_name}`);
+  const showProfile = (user: UserProfileType): void => {
+    console.log(`Showing profile of ${user.firstname} ${user.lastname}`);
     // TODO: Actually show the profile
   };
 
-  const renderUser = ({ item }: { item: User }): React.ReactElement => (
+  const renderUser = ({ item }: { item: UserProfileType }): React.ReactElement => (
     <View className="flex flex-row justify-between items-center p-4 border-b border-gray-200">
       <TouchableOpacity onPress={() => showProfile(item)}>
         <Text className="text-lg text-black">
-          {item.first_name + " " + item.last_name}
+          {item.firstname + " " + item.lastname}
         </Text>
       </TouchableOpacity>
       <CustomButton
         title="Chat"
         onPress={() => startChat(item)}
-        disabled={!item.id || !item.first_name || !item.last_name}
+        disabled={!item.id || !item.firstname || !item.lastname}
         className="w-14 h-8 rounded-md"
         fontSize="sm"
         padding="0"
@@ -51,11 +51,11 @@ const NewConversation = (): React.ReactElement => {
   // Simulating API call to fetch users
   useEffect(() => {
     const fetchUsers = async (): Promise<void> => {
-      const fetchedUsers: User[] = [
-        { id: 1, first_name: "John", last_name: "Doe" },
-        { id: 2, first_name: "Jane", last_name: "Smith" },
-        { id: 3, first_name: "This", last_name: "is" },
-        { id: 4, first_name: "a", last_name: "placeholder" },
+      const fetchedUsers: UserProfileType[] = [
+        { id: 1, firstname: "John", lastname: "Doe" },
+        { id: 2, firstname: "Jane", lastname: "Smith" },
+        { id: 3, firstname: "This", lastname: "is" },
+        { id: 4, firstname: "a", lastname: "placeholder" },
         //TODO: actually get users from db
       ];
       setUsers(fetchedUsers);
