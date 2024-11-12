87de3303d41e3448c86708ee7c9ca551b0e110fc
Arthur <arthur.descrivan@gmail.com>
2024-11-12 09:24:38 -0500

    <Added the appropriate routing logic to the new-conversation screen>

---
+++ app/(api)/(chat)/getConversationThatWasJustCreated+api.ts
@@ -0,0 +1,60 @@
+import { neon } from "@neondatabase/serverless";
+import { ConversationItem } from "@/types/type";
+
+export async function GET(request: Request) {
+  try {
+    const sql = neon(`${process.env.DATABASE_URL}`);
+    const url = new URL(request.url);
+    const userId1 = url.searchParams.get("id1");
+    const userId2 = url.searchParams.get("id2");
+
+    console.log("Received GET request for conversations for user with ID: ", userId);
+
+  
+    const rawResponse = await sql`
+      SELECT 
+        c.id::text, 
+        c.clerk_id_1, 
+        c.clerk_id_2 as other_clerk_id,
+        c.last_message as "lastMessageContent", 
+        c.last_message_timestamp as "lastMessageTimestamp",
+        u_self.nicknames as nicknames
+        u2.username
+      FROM conversations c
+      LEFT JOIN users u2 ON c.clerk_id_2 = u2.clerk_id
+      LEFT JOIN users u_self ON u_self.clerk_id = ${userId1}
+      WHERE c.clerk_id_1 = ${userId1} 
+      AND c.clerk_id_2 = ${userId2}
+    `;
+
+    // Transform the raw response to match the interface
+    const conversations: ConversationItem[] = rawResponse.map(row => {
+      // Find nickname for the other user if it exists
+      const nicknames: string[][] = row.nicknames || [];
+      const nickname = nicknames.find(([clerkId]) => clerkId === row.other_clerk_id)?.[1];
+
+      return {
+        id: row.id,
+        name: nickname || row.username, // Use nickname if exists, otherwise use username
+        clerk_id: row.other_clerk_id,
+        lastMessageContent: row.lastMessageContent || null,
+        lastMessageTimestamp: row.lastMessageTimestamp ? 
+          row.lastMessageTimestamp.toISOString() : null,
+        nickname: row.nicknames
+      };
+    });
+
+    return new Response(JSON.stringify({ data: conversations }), {
+      status: 200,
+    });
+    
+  } catch (error) {
+    console.error(error);
+    return new Response(
+      JSON.stringify({ error: "Failed to fetch user conversations." }),
+      {
+        status: 500,
+      }
+    );
+  }
+}
--- app/(root)/(chat)/new-conversation.tsx
+++ app/(root)/(chat)/new-conversation.tsx
@@ -10,10 +10,12 @@ import {
   TextInput,
   TouchableOpacity,
   View,
+  KeyboardAvoidingView,
   Alert,
 } from "react-native";
 import { UserProfileType, UserNicknamePair} from "@/types/type";
 import { SafeAreaView } from "react-native-safe-area-context";
+import AntDesign from "@expo/vector-icons/AntDesign";
 
 
 const NewConversation = (): React.ReactElement => {
@@ -23,6 +25,7 @@ const NewConversation = (): React.ReactElement => {
   const router = useRouter();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
+  const [convId, setConvId] = useState<string | null>(null);
 
   const fetchUsers = async () => {
     setLoading(true);
@@ -58,7 +61,7 @@ const NewConversation = (): React.ReactElement => {
         
     }, []);
 
-  const filteredUsers = users.filter((user) =>
+  const filteredUsers = users.filter((user) => 
     user[1].includes(searchText.toLowerCase())
   );
   const checkIfChatExists = async (user2: UserNicknamePair) => {
@@ -78,6 +81,9 @@ const NewConversation = (): React.ReactElement => {
         throw new Error(response.error);
       }
       console.log("response: ", response.data.length);
+      if (response.data.length > 0){
+        router.push(`/(root)/(chat)/conversation?conversationId=${response.data[0].id}&otherClerkId=${user2[0]}&otherName=${user2[1]}`);
+      }
       return response.data.length > 0;
     } catch (err) {
       console.error("Failed to fetch user data:", err);
@@ -90,9 +96,10 @@ const NewConversation = (): React.ReactElement => {
     const exists = await checkIfChatExists(otherUser);
     console.log("conversationExists: ", exists);
     if (exists) {
-      Alert.alert("CHANGE THIS BEFORE MAKING A PR", "Chat already exists, with this user");
+      console.log("Chat already exists, sending user to conversation with Id: ", convId);
     }
     else {
+      setLoading(true);
       try {
         const response = await fetchAPI(
           `/(api)/(chat)/newConversation`,
@@ -105,20 +112,45 @@ const NewConversation = (): React.ReactElement => {
           }
         );
         if (response.error) {
-          console.log("Error fetching user data");
+          console.log("Error creating conversation");
           console.log("response data: ", response.data);
           console.log("response status: ", response.status);
           // console.log("response: ", response);
           throw new Error(response.error);
         }
-        console.log("response: ", response.data);
-        Alert.alert("CHANGE THIS BEFORE MAKING A PR", "Chat created successfully");
+        console.log("Chat was successfully created, attempting to get conversation information to push user there");
+        try {
+          const result = await fetchAPI(
+            `/(api)/(chat)/getConversationThatWasJustCreated?id1=${user!.id}&id2=${otherUser[0]}`,
+            {
+              method: "GET",
+            }
+          );
+          if (result.error) {
+            console.log("Error fetching conversation data");
+            console.log("response data: ", result.data);
+            console.log("response status: ", result.status);
+            // console.log("response: ", response);
+            throw new Error(result.error);
+          }
+          else {
+            const conversation = result.data[0];
+            console.log(`Pushing user to conversation that was just created with conversation ID: ${conversation.id}`);
+            router.push(`/(root)/(chat)/conversation?conversationId=${conversation.id}&otherClerkId=${conversation.clerk_id}&otherName=${conversation.name}`);
+          }
+        }
+        catch (err) {
+          console.error("Failed to fetch conversation data:", err);
+          setError("Chat was successfully created, but failed to send user to conversation.");
+        }
       } catch (err) {
         console.error("Failed to create new conversation:", err);
         setError("Failed to create new conversation");
       } 
+      finally {
+        setLoading(false);
+      }
     }
-    // TODO: Add your logic for initiating a chat here
   };
 
   const renderUser = ({ item }: { item: UserNicknamePair }): React.ReactElement => (
@@ -144,33 +176,40 @@ const NewConversation = (): React.ReactElement => {
     </View>
   );
 
-  // Simulating API call to fetch users
-
   return (
     <SafeAreaView className="flex-1">
       <SignedIn>
-        <View className="flex-1 bg-gray-100">
-          <TextInput
-            className="h-11 mx-4 px-4 rounded-lg border border-gray-300 text-base focus:outline-none focus:border-blue-500 focus:ring-blue-500"
-            placeholder="Search users..."
-            placeholderTextColor="#4a4a4a"
-            value={searchText}
-            onChangeText={(text): void => setSearchText(text)}
-          />
-          {loading ? (
-            <View className="flex-[0.8] justify-center items-center">
-            <ActivityIndicator size="large" color="black" />
+        <KeyboardAvoidingView behavior={"padding"} style={{ flex: 1 }}>
+          <View className="flex-1 bg-gray-100">
+            <View className="flex flex-row items-center justify-between px-4 pt-2 pr-9">
+              <View className="mr-2">
+                <TouchableOpacity onPress={() => router.replace("/(root)/(tabs)/chat")}>
+                  <AntDesign name="caretleft" size={18} color="0076e3" />
+                </TouchableOpacity>
+              </View>
+              <TextInput
+                className="h-11 mx-4 px-4 rounded-lg border border-gray-300 text-base focus:outline-none focus:border-blue-500 focus:ring-blue-500"
+                placeholder="Search users..."
+                placeholderTextColor="#4a4a4a"
+                value={searchText}
+                onChangeText={(text): void => setSearchText(text)}
+              />
+            </View>
+            {loading ? (
+              <View className="flex-[0.8] justify-center items-center">
+              <ActivityIndicator size="large" color="black" />
+            </View>
+          ) : error ? (
+            <Text>{error}</Text>
+          ) : (
+            <FlatList
+              data={filteredUsers}
+              renderItem={renderUser}
+              keyExtractor={(item): string => String(item[0])}
+            />)
+          }
           </View>
-        ) : error ? (
-          <Text>{error}</Text>
-        ) : (
-          <FlatList
-            data={filteredUsers}
-            renderItem={renderUser}
-            keyExtractor={(item): string => String(item[0])}
-          />)
-        }
-        </View>
+        </KeyboardAvoidingView>
       </SignedIn>
     </SafeAreaView>
   );
