5dcca65d88283dfc4d02e6268d4fe5c61bacf06b
Evelyn Tromp <evelynktromp@gmail.com>
2024-11-25 16:28:14 -0500

    The app is functional. You can click on userprofile and it won't cause an error

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -1,5 +1,4 @@
 import CustomButton from "@/components/CustomButton";
-import Circle from "@/components/Circle";
 import { useNavigationContext } from "@/components/NavigationContext";
 import PostGallery from "@/components/PostGallery";
 import { icons } from "@/constants/index";
@@ -12,14 +11,11 @@ import {
   UserProfileType,
 } from "@/types/type";
 import { useUser } from "@clerk/clerk-expo";
-import AntDesign from "@expo/vector-icons/AntDesign";
-import { useRoute } from "@react-navigation/native";
 import { useRouter } from "expo-router";
 import { useEffect, useState } from "react";
 import {
   ActivityIndicator,
   Image,
-  ImageSourcePropType,
   Pressable,
   Text,
   TextInput,
@@ -37,9 +33,7 @@ const UserProfile: React.FC<UserProfileProps> = ({ userId, onSignOut }) => {
   const [profileUser, setProfileUser] = useState<UserProfileType | null>(null);
   const [userPosts, setUserPosts] = useState<Post[]>([]);
   const { stateVars, setStateVars } = useNavigationContext();
-  const route = useRoute();
   const router = useRouter();
-  const currentScreen = route.name as string;
   const [currentSubscreen, setCurrentSubscreen] = useState<string>("posts");
 
   const isEditable = user!.id === userId;
@@ -54,7 +48,6 @@ const UserProfile: React.FC<UserProfileProps> = ({ userId, onSignOut }) => {
 
   const fetchCurrentNickname = async () => {
     try {
-      // console.log("user: ", user!.id);
       const response = await fetchAPI(
         `/(api)/(users)/getUserInfo?id=${user!.id}`,
         {
@@ -62,13 +55,8 @@ const UserProfile: React.FC<UserProfileProps> = ({ userId, onSignOut }) => {
         }
       );
       if (response.error) {
-        console.log("Error fetching user data");
-        console.log("response data: ", response.data);
-        console.log("response status: ", response.status);
-        // console.log("response: ", response);
         throw new Error(response.error);
       }
-      // console.log("response: ", response.data[0].nicknames);
       const nicknames = response.data[0].nicknames || [];
       return findUserNickname(nicknames, userId) === -1
         ? ""
@@ -77,6 +65,7 @@ const UserProfile: React.FC<UserProfileProps> = ({ userId, onSignOut }) => {
       console.error("Failed to fetch user data:", error);
     }
   };
+
   useEffect(() => {
     const getData = async () => {
       const data = await fetchCurrentNickname();
@@ -89,7 +78,7 @@ const UserProfile: React.FC<UserProfileProps> = ({ userId, onSignOut }) => {
     if (isEditable) {
       setStateVars({
         ...stateVars,
-        previousScreen: currentScreen,
+        previousScreen: "profile",
       });
       router.push("/(root)/(location)/country");
     }
@@ -120,10 +109,9 @@ const UserProfile: React.FC<UserProfileProps> = ({ userId, onSignOut }) => {
   };
 
   const handleAddNickname = () => {
-    console.log("Add Nickname button was pressed:");
     setStateVars({
       ...stateVars,
-      previousScreen: currentScreen,
+      previousScreen: "profile",
       userId,
     });
     router.push("/(root)/(profile)/nickname");
@@ -153,15 +141,8 @@ const UserProfile: React.FC<UserProfileProps> = ({ userId, onSignOut }) => {
     <View className="flex-1 mt-3">
       <View className="mx-7 mb-2">
         <View className="flex flex-row items-center justify-between">
-          {!isEditable && (
-            <View className="mr-2">
-              <TouchableOpacity onPress={() => router.back()}>
-                <AntDesign name="caretleft" size={18} color="0076e3" />
-              </TouchableOpacity>
-            </View>
-          )}
           <Text className={`text-2xl font-JakartaBold flex-1`}>
-            {nickname ? nickname : profileUser?.username ? `${profileUser?.username}`:`${profileUser?.firstname?.charAt(0)}.`}
+            {nickname ? nickname : profileUser?.username ? `${profileUser?.username}` : `${profileUser?.firstname?.charAt(0)}.`}
           </Text>
 
           <View className="flex flex-row items-center">
@@ -194,7 +175,7 @@ const UserProfile: React.FC<UserProfileProps> = ({ userId, onSignOut }) => {
             </Pressable>
           ) : (
             <Text className="text-gray-500 font-Jakarta text-base mt-3">
-              📍{profileUser?.city}, {profileUser?.state},{" "}
+              📍{profileUser?.city}, {profileUser?.state}, {" "}
               {profileUser?.country}
             </Text>
           )}
@@ -223,40 +204,43 @@ const UserProfile: React.FC<UserProfileProps> = ({ userId, onSignOut }) => {
       <View className="mx-4 my-4">
         <View className="border-t border-gray-200" />
       </View>
+      
       <View
-      className="flex flex-row justify-around bg-transparent rounded-full p-2"
-      style={{ width: '60%', alignSelf: 'center' }}
-    >
-      <TouchableOpacity
-        onPress={() => setCurrentSubscreen('posts')}
-        className={`py-2.5 px-4 rounded-full ${
-          currentSubscreen === 'posts' ? 'bg-gray-300' : ''
-        }`}
+        className="flex flex-row justify-around bg-transparent rounded-full p-2"
+        style={{ width: '60%', alignSelf: 'center' }}
       >
-        <Image
-          source={icons.home}
-          tintColor= {currentSubscreen==="posts" ? "#ffe640" : "#e0e0e0"}
-          resizeMode="contain"
-          className="w-6 h-6"
-        />
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => setCurrentSubscreen('colors')}
-        className={`p-2 rounded-full ${
-          currentSubscreen === 'colors' ? 'bg-gray-300' : ''
-        }`}
-      >
-        <Image
-          source={icons.palette}
-          tintColor= {currentSubscreen==="colors" ? undefined : "#e0e0e0"}
-          resizeMode="contain"
-          className="w-6 h-6"
-        />
-      </TouchableOpacity>
-    </View>
-    <View className="mx-4 my-4">
+        <TouchableOpacity
+          onPress={() => setCurrentSubscreen('posts')}
+          className={`py-2.5 px-4 rounded-full ${
+            currentSubscreen === 'posts' ? 'bg-gray-300' : ''
+          }`}
+        >
+          <Image
+            source={icons.home}
+            tintColor={currentSubscreen === "posts" ? "#ffe640" : "#e0e0e0"}
+            resizeMode="contain"
+            className="w-6 h-6"
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => setCurrentSubscreen('colors')}
+          className={`p-2 rounded-full ${
+            currentSubscreen === 'colors' ? 'bg-gray-300' : ''
+          }`}
+        >
+          <Image
+            source={icons.palette}
+            tintColor={currentSubscreen === "colors" ? undefined : "#e0e0e0"}
+            resizeMode="contain"
+            className="w-6 h-6"
+          />
+        </TouchableOpacity>
+      </View>
+      
+      <View className="mx-4 my-4">
         <View className="border-t border-gray-200" />
       </View>
+      
       <View className="items-center flex-1">
         {currentSubscreen === "colors" ? (
           <View className="items-center">
@@ -278,10 +262,10 @@ const UserProfile: React.FC<UserProfileProps> = ({ userId, onSignOut }) => {
               handleUpdate={fetchUserData} 
             />
           </View>
-        )
-        }
+        )}
       </View>
-      {currentScreen === "profile" && <View className="min-h-[80px]" />}
+      
+      <View className="min-h-[80px]" />
     </View>
   );
 };
