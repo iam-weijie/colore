4f3e9d3d14daf7ae5ecef2d1c30c2057193c5e25
mintary <ewang0332@gmail.com>
2025-01-21 13:52:18 -0500

    fixed positioning of dropdown menu

--- components/DropdownMenu.tsx
+++ components/DropdownMenu.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Modal, Pressable, Text, TouchableOpacity, View } from "react-native";
 
 type MenuItem = {
@@ -8,14 +8,27 @@ type MenuItem = {
 
 type DropdownMenuProps = {
   menuItems: MenuItem[];
+  customMenuWidth?: number;
 }
 
-const DropdownMenu: React.FC<DropdownMenuProps> = ({ menuItems }) => {
+const DropdownMenu: React.FC<DropdownMenuProps> = ({ menuItems, customMenuWidth }) => {
   const [visible, setVisible] = useState(false);
+  const [menuPosition, setMenuPosition] = useState({ top: 0, left: 0});
+  const triggerRef = useRef<View>(null); // get position of trigger element
+  const menuWidth = customMenuWidth ? customMenuWidth : 100;
+
+  const handlePress = () => {
+    if (triggerRef.current) {
+      triggerRef.current.measure((fx, fy, width, height, pageX, pageY) => {
+        setMenuPosition({ top: pageY + height, left: pageX + width - menuWidth});
+        setVisible(true);
+      });
+    }
+  }
 
   return (
-    <View>
-      <TouchableOpacity onPress={() => setVisible(true)}>
+    <View className="relative">
+      <TouchableOpacity onPress={handlePress} ref={triggerRef}>
         <View className="flex flex-row justify-center items-center h-6 w-6">
           <View className="w-1 h-1 bg-gray-600 rounded-full mr-1" />
           <View className="w-1 h-1 bg-gray-600 rounded-full mr-1" />
@@ -32,15 +45,18 @@ const DropdownMenu: React.FC<DropdownMenuProps> = ({ menuItems }) => {
           className="flex-1 bg-black/30"
           onPress={() => setVisible(false)}
         >
-          <View className="absolute top-16 right-8 bg-white rounded-lg shadow-lg">           
+          <View 
+            style={{ top: menuPosition.top, left: menuPosition.left, width: menuWidth }}
+            className="bg-white rounded-lg shadow-lg"
+          >           
             {menuItems.map((item, index) => (
               <TouchableOpacity
                 key={index}
-                onPress={async () => {
+                onPress={() => {
                   setVisible(false);
                   item.onPress();
                 }}
-                className="flex-row items-center px-4 py-3 border-b border-gray-200"
+                className={`flex-row items-center px-4 py-3 ${index < menuItems.length - 1 ? "border-b border-gray-200" : ""}`}
               >
                 <Text className="font-Jakarta text-base">{item.label}</Text>
               </TouchableOpacity>
