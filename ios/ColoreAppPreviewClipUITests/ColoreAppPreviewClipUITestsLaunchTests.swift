d5cf6434b6d83fe36d903ae6f688e7a540f7f9b3
Damien K. D <kd.pdamien.bc@icloud.com>
2025-03-05 09:27:12 -0500

    feat: add App Clip support and integrate tracking transparency library

---
+++ ios/ColoreAppPreviewClipUITests/ColoreAppPreviewClipUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ColoreAppPreviewClipUITestsLaunchTests.swift
+//  ColoreAppPreviewClipUITests
+//
+//  Created by Pierre Damien Kemka Douanla on 2025-03-04.
+//
+
+import XCTest
+
+final class ColoreAppPreviewClipUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
