6dc7fe1fd890ca237a7e03f66c98911b8041f948
Damien K. D <kd.pdamien.bc@icloud.com>
2025-03-22 11:00:58 -0400

    Enhance: Add animations to new-post and edit-post screens

--- app/root/_layout.tsx
+++ app/root/_layout.tsx
@@ -4,13 +4,13 @@ const Layout = () => {
 
   return (
     <Stack>
-      <Stack.Screen name="tabs" options={{ headerShown: false }} />
+      <Stack.Screen name="tabs" options={{ headerShown: false, animation: 'none' }} />
       <Stack.Screen name="chat" options={{ headerShown: false }} />
       <Stack.Screen name="user-info" options={{ headerShown: false }} />
-      <Stack.Screen name="new-post" options={{ headerShown: false }} />
-      <Stack.Screen name="preview-post" options={{ headerShown: false }} />
+      <Stack.Screen name="new-post" options={{ headerShown: false, animation: 'fade', animationDuration: 200  }} />
+      <Stack.Screen name="preview-post" options={{ headerShown: false, animation: 'none'  }} />
       <Stack.Screen name="saved-post-gallery" options={{ headerShown: false }} />
-      <Stack.Screen name="edit-post" options={{ headerShown: false }} />
+      <Stack.Screen name="edit-post" options={{ headerShown: false, animation: 'fade', animationDuration: 300  }} />
       <Stack.Screen name="location" options={{ headerShown: false }} />
       <Stack.Screen name="profile" options={{ headerShown: false }} />
       <Stack.Screen name="post" options={{ headerShown: false }} />
